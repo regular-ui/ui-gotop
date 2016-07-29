1b1813fce1b16227bec3cadee6635680a95eab01
Yusen <rainforest92@126.com>
2016-07-29 18:46:09 +0800

    [added] Gotop

---
+++ README.md
@@ -0,0 +1,29 @@
+# ui-gotop
+
+Regular UI - Gotop
+
+[![NPM Version][npm-img]][npm-url]
+[![Build Status][travis-img]][travis-url]
+[![Test Coverage][coveralls-img]][coveralls-url]
+[![Dependencies][david-img]][david-url]
+[![NPM Download][download-img]][download-url]
+
+[npm-img]: http://img.shields.io/npm/v/rgui-ui-gotop.svg?style=flat-square
+[npm-url]: http://npmjs.org/package/rgui-ui-gotop
+[travis-img]: https://img.shields.io/travis/regular-ui/ui-gotop.svg?style=flat-square
+[travis-url]: https://travis-ci.org/regular-ui/ui-gotop
+[coveralls-img]: https://img.shields.io/coveralls/regular-ui/ui-gotop.svg?style=flat-square
+[coveralls-url]: https://coveralls.io/r/regular-ui/ui-gotop
+[david-img]: http://img.shields.io/david/regular-ui/ui-gotop.svg?style=flat-square
+[david-url]: https://david-dm.org/regular-ui/ui-gotop
+[download-img]: https://img.shields.io/npm/dm/rgui-ui-gotop.svg?style=flat-square
+[download-url]: https://npmjs.org/package/rgui-ui-gotop
+
+## Gotop（回到顶部）
+
+- 基础示例+API: https://regular-ui.github.io/ui-gotop/gotop/index.html
+- 进阶案例: https://regular-ui.github.io/ui-gotop/gotop/case.html
+
+## Copyright and License
+
+Code released under the MIT license.
---
+++ gotop/index.js
@@ -0,0 +1,66 @@
+import { Component } from 'rgui-ui-base';
+import template from './index.rgl';
+import { dom } from 'regularjs';
+
+/**
+ * @class Gotop
+ * @extend Component
+ * @param {object}                  options.data                     =  绑定属性
+ * @param {boolean=false}           options.data.disabled            => 是否禁用
+ * @param {boolean=true}            options.data.visible             => 是否显示
+ * @param {string=''}               options.data.class               => 补充class
+ */
+const Gotop = Component.extend({
+    name: 'gotop',
+    template,
+    /**
+     * @protected
+     * @override
+     */
+    config() {
+        this.data = Object.assign({
+            position: 'bottomright',
+            scrollParent: document.body,
+            hideBeforeScroll: true,
+        }, this.data);
+        this.supr();
+
+        if (this.data.hideBeforeScroll)
+            this.data.visible = false;
+
+        this._onScroll = this._onScroll.bind(this);
+    },
+    /**
+     * @protected
+     * @override
+     */
+    init() {
+        this.supr();
+        dom.on(this.data.scrollParent, 'scroll', this._onScroll);
+    },
+    /**
+     * @protected
+     * @override
+     */
+    destroy() {
+        dom.off(this.data.scrollParent, 'scroll', this._onScroll);
+        this.supr();
+    },
+    /**
+     * @method gotop() 回到顶部
+     * @public
+     * @return {void}
+     */
+    gotop() {
+        if (this.data.disabled)
+            return;
+
+        this.data.scrollParent = 0;
+    },
+    _onScroll() {
+        if (this.data.scrollParent.scrollTop)
+            this.data.visible = true;
+    },
+});
+
+export default Gotop;
